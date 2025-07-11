8520e48e5690952036c36de6df8a489f214eb3f0
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-07-11 12:21:23 -0500

    Adding front-routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./web-front/web-front.routes'),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/app/web-front/web-front.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+export const WebFrontRoutes: Routes = [];
+
+export default WebFrontRoutes;
