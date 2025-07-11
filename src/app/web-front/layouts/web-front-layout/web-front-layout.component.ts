6ab8c9b46ebd25437ecf6957a0f9aab650d9f872
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-07-11 12:49:14 -0500

    Adding layout component and configuring routes

---
+++ src/app/web-front/layouts/web-front-layout/web-front-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-front-layout',
+  imports: [],
+  templateUrl: './web-front-layout.component.html',
+})
+export class WebFrontLayoutComponent { }
--- src/app/web-front/web-front.routes.ts
+++ src/app/web-front/web-front.routes.ts
@@ -1,5 +1,20 @@
 import { Routes } from '@angular/router';
+import { WebFrontLayoutComponent } from './layouts/web-front-layout/web-front-layout.component';
 
-export const WebFrontRoutes: Routes = [];
+export const WebFrontRoutes: Routes = [
+  {
+    path: '',
+    component: WebFrontLayoutComponent,
+    // children: [
+    //   {
+    //     path: 'location',
+    //   },
+    // ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 export default WebFrontRoutes;
