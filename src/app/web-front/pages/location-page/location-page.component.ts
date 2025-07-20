acbcba9e04c58f4da0717bbf8079aa7f88225849
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-07-20 15:31:36 -0500

    Adding reactivity and styles to the temperature unit and checkbox

--- src/app/web-front/pages/location-page/location-page.component.ts
+++ src/app/web-front/pages/location-page/location-page.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'location-page',
   imports: [],
   templateUrl: './location-page.component.html',
 })
-export class LocationPageComponent {}
+export class LocationPageComponent {
+  public temperatureUnit = signal<'c' | 'f'>('c');
+
+  toggleUnit(event: Event): void {
+    const tempInput = event.target as HTMLInputElement;
+    this.temperatureUnit.set(tempInput.checked ? 'f' : 'c');
+  }
+}
