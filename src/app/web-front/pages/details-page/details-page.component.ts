8c82b60d3e6b50232689a95b728a188dc823a8af
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-09-15 16:40:47 -0500

    Creating forecast details page component and configuring details route

---
+++ src/app/web-front/pages/details-page/details-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'details-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-page.component.html',
+})
+export class DetailsPageComponent {}
--- src/app/web-front/web-front.routes.ts
+++ src/app/web-front/web-front.routes.ts
@@ -4,6 +4,7 @@ import { LocationPageComponent } from './pages/location-page/location-page.compo
 import { ForecastPageComponent } from './pages/forecast-page/forecast-page.component';
 import { WebFrontLayoutComponent } from './layouts/web-front-layout/web-front-layout.component';
 import { MissingLocationGuard } from '@shared/guards/missing-location.guard';
+import { DetailsPageComponent } from './pages/details-page/details-page.component';
 
 export const WebFrontRoutes: Routes = [
   {
@@ -16,8 +17,17 @@ export const WebFrontRoutes: Routes = [
       },
       {
         path: 'forecast',
-        component: ForecastPageComponent,
-        canMatch: [MissingLocationGuard],
+        children: [
+          {
+            path: '',
+            component: ForecastPageComponent,
+            canMatch: [MissingLocationGuard],
+          },
+          {
+            path: 'details',
+            component: DetailsPageComponent,
+          },
+        ],
       },
       {
         path: '**',
