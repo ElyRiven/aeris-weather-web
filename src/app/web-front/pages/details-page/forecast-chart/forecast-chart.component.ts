c9d97627d7345f06c301e84460f7a7a3e0ad3405
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-09-17 16:48:24 -0500

    Working on chart design

--- src/app/web-front/pages/details-page/forecast-chart/forecast-chart.component.ts
+++ src/app/web-front/pages/details-page/forecast-chart/forecast-chart.component.ts
@@ -2,13 +2,13 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  OnInit,
   signal,
   viewChild,
 } from '@angular/core';
 
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
+Chart.defaults.font.family = 'jost';
 
 @Component({
   selector: 'forecast-chart',
@@ -31,7 +31,7 @@ export class ForecastChartComponent implements AfterViewInit {
 
     this.chart.set(
       new Chart(canvas, {
-        type: 'bar',
+        type: 'line',
         data: {
           labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
           datasets: [
@@ -39,13 +39,50 @@ export class ForecastChartComponent implements AfterViewInit {
               label: '# of Votes',
               data: [12, 19, 3, 5, 2, 3],
               borderWidth: 1,
+              pointRadius: 6,
             },
           ],
         },
         options: {
+          responsive: true,
           scales: {
             y: {
               beginAtZero: true,
+              grid: {
+                display: false,
+              },
+            },
+            x: {
+              //* HIDES TICKS IN THE X AXIS (TEXT LABELS ON EACH POINT)
+              // ticks: {
+              //   display: false,
+              // },
+              grid: {
+                display: false,
+              },
+            },
+          },
+          plugins: {
+            legend: {
+              display: true,
+              position: 'bottom',
+              align: 'center',
+              labels: {
+                color: 'oklch(0.5956 0.0381 257.87)',
+                boxHeight: 3,
+                boxWidth: 12,
+              },
+            },
+            title: {
+              text: 'Light Rain',
+              display: true,
+              position: 'top',
+              align: 'center',
+              font: {
+                weight: 600,
+                size: 18,
+              },
+              color: 'oklch(0.3351 0.0331 260.91)',
             },
           },
         },
