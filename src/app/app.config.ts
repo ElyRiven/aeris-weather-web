c756e06d87352154cca3c277563a2282abe33d24
Elizabeth Tamayo <dianelizbty@gmail.com>
2025-09-25 18:29:04 -0500

    Adding view transitions and hash location to the app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withHashLocation,
+  withViewTransitions,
+} from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
@@ -8,8 +12,8 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideAnimations(),
+    provideRouter(routes, withHashLocation(), withViewTransitions()),
     provideHttpClient(withFetch()),
+    provideAnimations(),
   ],
 };
